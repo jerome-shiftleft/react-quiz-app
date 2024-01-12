6c295773aceb6839cdd01b58cc67700a9027eb24
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-12 18:40:16 +0800

    quiz complete

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,49 @@
-import { useState } from "react"
+import { useState } from "react";
+import QUESTIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([])
+  //const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+  console.log(`activeQuestionIndex: ${activeQuestionIndex}`);
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  function handSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy Icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <>
-      <p>Currently active Question</p>
+      <div id="quiz">
+        <div id="question">
+          <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+          <ul id="answers">
+            {shuffledAnswers.map((answer) => (
+              <li key={answer} className="answer">
+                <button onClick={() => handSelectAnswer(answer)}>
+                  {answer}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
-  )
+  );
 }
